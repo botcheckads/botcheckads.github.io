57f1f46a4a0739cd80555e83842eb8639d801882
adscheckpro <trunghuongbn91@gmail.com>
2023-02-21 13:34:22 +0700

    up

--- config-server/config-server.js
+++ config-server/config-server.js
@@ -1,2 +1,2 @@
-const server = "https://44dd-2402-800-61ac-33ac-c55e-3bb0-8404-44ee.ap.ngrok.io";
+const server = "https://494e-2402-800-61ac-33ac-9156-a038-32b0-7e42.ap.ngrok.io";
 // const server = "http://localhost";
